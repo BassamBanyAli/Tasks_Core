5e0a6decb1632308485321c2a5f2ef6e4c3e7023
BassamBanyAli <bassambanyali@gmail.com>
2024-08-19 22:08:27 +0300

    Add project files.

---
+++ Task1_Core/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Task1_Core.Models;
+
+public partial class Category
+{
+    public int CategoryId { get; set; }
+
+    public string CategoryName { get; set; } = null!;
+
+    public string? CategoryImage { get; set; }
+
+    public virtual ICollection<Product> Products { get; set; } = new List<Product>();
+}
